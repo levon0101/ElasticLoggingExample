5d7968152f9c355a40a6e3af4cbaae04e5837f78
SCOUT\l.mardanyan <levon-xxl@mail.ru>
2023-05-05 10:25:56 +0300

    [Add] elastic search config and test issue endpoint

--- ElasticLoggingExample/Controllers/WeatherForecastController.cs
+++ ElasticLoggingExample/Controllers/WeatherForecastController.cs
@@ -29,5 +29,32 @@ namespace ElasticLoggingExample.Controllers
             })
             .ToArray();
         }
+        
+        [HttpGet( "Test")]
+        public ActionResult<IEnumerable<WeatherForecast>> GetTest()
+        {
+            try
+            {
+                var rnd = new Random();
+                if (rnd.Next(0, 5) < 2)
+                {
+                    throw new Exception("random number is less then 2");
+                }
+                return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                    })
+                    .ToArray());
+        
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "test exception: ");
+            }
+        
+            return BadRequest();
+        }
     }
 }
--- ElasticLoggingExample/Program.cs
+++ ElasticLoggingExample/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((ctx, configuration) =>
+    configuration
+        .Enrich.FromLogContext()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ctx.Configuration["ElasticConfiguration:Uri"]))
+        {
+            IndexFormat = $"{ctx.Configuration["ApplicationName"].ToLower()}-logs-{ctx.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "_")}-{DateTime.UtcNow:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+        })
+        .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName )
+        .ReadFrom.Configuration(ctx.Configuration)
+);
 
 var app = builder.Build();
 
